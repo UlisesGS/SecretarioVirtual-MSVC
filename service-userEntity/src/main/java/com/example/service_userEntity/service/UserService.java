ec31e2a32d6fd0ed76975839fe4ced31a0b2e9bd
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-27 13:21:30 -0300

    Inyeccion de UserRepository y modificacion de metodos para usar el mismo

--- service-userEntity/src/main/java/com/example/service_userEntity/service/UserService.java
+++ service-userEntity/src/main/java/com/example/service_userEntity/service/UserService.java
@@ -1,37 +1,36 @@
 package com.example.service_userEntity.service;
 
 import com.example.service_userEntity.model.UserEntity;
+import com.example.service_userEntity.repositoy.IUserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
+    @Autowired
+    private IUserRepository userRepository;
+
     private List<UserEntity> userList=new ArrayList<>();
 
-    public UserService(){
-        userList.add(new UserEntity("1", "Daniel", "daniel@example.com","user123"));
-        userList.add(new UserEntity("2", "Ulises", "ulises@example.com","user123"));
+    @PostConstruct
+    public void initUsers() {
+        userRepository.save(new UserEntity("1", "Daniel", "daniel@example.com", "user123"));
+        userRepository.save(new UserEntity("2", "Ulises", "ulises@example.com", "user123"));
     }
-
     public List<UserEntity> getAllUsers() {
-        return userList;
+        return userRepository.findAll();
     }
 
-    public UserEntity getUserById(Long id){
-        return userList.stream()
-                .filter(user->user.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+    public UserEntity getUserById(String id){
+        return userRepository.findById(id)
+                .orElseThrow(()-> new NoSuchElementException ("User with id: " + id + " not found"));
     }
 
-    public UserEntity createUser(UserEntity user) {
-        userList.add(user);
-        return userList.getLast();
-    }
 
-    public boolean removeUserById(Long id) {
-        return userList.removeIf(user -> user.getId().equals(id));
-    }
+
 }
