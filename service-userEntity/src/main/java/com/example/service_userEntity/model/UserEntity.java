8bb5175a1daf874a285376528407a908b5043415
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-27 13:19:41 -0300

    implementacion de anotaciones para el id y el nombre de la coleccion

--- service-userEntity/src/main/java/com/example/service_userEntity/model/UserEntity.java
+++ service-userEntity/src/main/java/com/example/service_userEntity/model/UserEntity.java
@@ -7,12 +7,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document(collection = "UserEntity")
 public class UserEntity {
+    @Id
     private String id;
     @NotBlank
     private String username;
