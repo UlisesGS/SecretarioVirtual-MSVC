f901d5452230473d05da9ee660b248e46e2bdddc
Daniel-iwach <daniel.g.iwach@gmail.com>
2025-07-30 19:48:02 -0300

    cambios de configuracion de seguridad

--- service-security/src/main/java/com/example/service_security/controller/AuthController.java
+++ service-security/src/main/java/com/example/service_security/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.example.service_security.dto.ResponseLoginDto;
 import com.example.service_security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +22,6 @@ public class AuthController {
 
     @PostMapping("/auth/login")
     public ResponseEntity<ResponseLoginDto> login(@RequestBody RequestLoginDto request) {
-        return ResponseEntity.ok(authService.login(request));
+        return new ResponseEntity<>(authService.login(request), HttpStatus.OK);
     }
 }
