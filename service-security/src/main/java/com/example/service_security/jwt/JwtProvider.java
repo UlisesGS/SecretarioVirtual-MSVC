753729c16b4042ff5958a02c1b7695411755c707
UlisesGS <ulisesgadea28@gmail.com>
2025-08-04 21:28:25 -0300

    dejando lista la seguridad en dos capas

--- service-security/src/main/java/com/example/service_security/jwt/JwtProvider.java
+++ service-security/src/main/java/com/example/service_security/jwt/JwtProvider.java
@@ -1,10 +1,8 @@
 package com.example.service_security.jwt;
 
 import io.jsonwebtoken.*;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.*;
